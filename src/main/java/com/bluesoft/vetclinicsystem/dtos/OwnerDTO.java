4eab7527656278a734fdd7fc31e4da3207c91fa8
MohamedHesham <mohamed.hesham@sawa-labs.com>
2021-12-02 17:54:23 +0200

    - Validation on owner email

--- src/main/java/com/bluesoft/vetclinicsystem/dtos/OwnerDTO.java
+++ src/main/java/com/bluesoft/vetclinicsystem/dtos/OwnerDTO.java
@@ -1,5 +1,6 @@
 package com.bluesoft.vetclinicsystem.dtos;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -13,6 +14,7 @@ public class OwnerDTO {
     @Size(max = 50, message = "Owner name max size is 50")
     private String name;
 
+    @Email(message = "Email is not valid", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
     @NotNull(message = "Owner email is required")
     @NotBlank(message = "Owner email is required")
     @Size(max = 100, message = "Owner email max size is 50")
