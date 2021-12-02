98e6020e7fc45386b040a2a8e400091bbc6302e9
MohamedHesham <mohamed.hesham@sawa-labs.com>
2021-12-02 17:56:13 +0200

    - Validation on owner and clinic emails

--- src/main/java/com/bluesoft/vetclinicsystem/dtos/ClinicDTO.java
+++ src/main/java/com/bluesoft/vetclinicsystem/dtos/ClinicDTO.java
@@ -1,5 +1,6 @@
 package com.bluesoft.vetclinicsystem.dtos;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -26,6 +27,7 @@ public class ClinicDTO {
     @Size(max = 100, message = "Clinic workingDaysHours max size is 11")
     private String workingDaysHours;
 
+    @Email(message = "Email is not valid", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
     @Size(max = 100, message = "Clinic email max size is 11")
     private String email;
 
